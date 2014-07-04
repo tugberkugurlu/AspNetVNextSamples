8980d36fb415279745ee7955567f28f4925816ed
tugberkugurlu <tugberkugurlu@gmail.com>
2014-07-04 11:31:51 +0300

    started ViewComponentSample

---
+++ src/ViewComponentSample/Startup.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Routing;
+using Microsoft.Framework.DependencyInjection;
+
+namespace ViewComponentSample 
+{
+    public class Startup 
+    {
+        public void Configure(IBuilder app)
+        {
+            app.UseErrorPage();
+            
+            app.UseServices(services => 
+            {
+                services.AddMvc();
+            });
+            
+            app.UseMvc(routes => 
+            {
+                routes.MapRoute(
+                    "controllerActionRoute",
+                    "{controller}/{action}",
+                    new { controller = "Home", action = "Index" });
+            });
+        }
+    }
+}
