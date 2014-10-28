bd2ed69082d17b9b4e79e8ae338667d3dd6497cc
tugberkugurlu <tugberkugurlu@gmail.com>
2014-10-28 12:43:40 +0200

    added LoggingWithOldConsole sample

---
+++ classic/LoggingWithOldConsole/LoggingWithOldConsole/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.Framework.Logging;
+
+namespace LoggingWithOldConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILoggerFactory loggerFactory = new LoggerFactory();
+            loggerFactory.AddProvider(new DiagnosticsLoggerProvider());
+            FooManager manager = new FooManager(loggerFactory);
+            manager.Run();
+        }
+    }
+
+    public class FooManager
+    {
+        private readonly ILogger _logger;
+
+        public FooManager(ILoggerFactory loggerFactory)
+        {
+            if (loggerFactory == null)
+            {
+                throw new ArgumentNullException("loggerFactory");
+            }
+
+            _logger = loggerFactory.Create<FooManager>();
+        }
+
+        public void Run()
+        {
+            _logger.WriteVerbose("Writing verbose...");
+            _logger.WriteInformation("Writing info...");
+            _logger.WriteWarning("Writing warning...");
+            _logger.WriteError("Writing error...");
+            _logger.WriteCritical("Writing critical...");
+        }
+    }
+}
