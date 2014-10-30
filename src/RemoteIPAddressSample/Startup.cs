2c54fc731b3b2d46f0b622756d814b42f91ae58f
tugberkugurlu <tugberkugurlu@gmail.com>
2014-10-30 11:35:53 +0200

    doing null check for IPAddress retriaval

--- src/RemoteIPAddressSample/Startup.cs
+++ src/RemoteIPAddressSample/Startup.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNet.HttpFeature;
-using System.Net;
 
 namespace RemoteIPAddressSample
 {
@@ -15,12 +14,11 @@ namespace RemoteIPAddressSample
                 // https://github.com/aspnet/HttpAbstractions/blob/fee220569aa108078ab0e231080724eb74ec8b2d/src/Microsoft.AspNet.Http/HttpContext.cs#L45-L48
                 // https://github.com/aspnet/HttpAbstractions/blob/fee220569aa108078ab0e231080724eb74ec8b2d/src/Microsoft.AspNet.HttpFeature/IHttpConnectionFeature.cs
                 IHttpConnectionFeature connection = ctx.GetFeature<IHttpConnectionFeature>();
+                string ipAddress = connection != null
+                    ? connection.RemoteIpAddress.ToString()
+                    : null;
 
-                // https://github.com/aspnet/HttpAbstractions/blob/fee220569aa108078ab0e231080724eb74ec8b2d/src/Microsoft.AspNet.HttpFeature/IHttpConnectionFeature.cs#L12
-                // TODO: Should I do null check here?
-                IPAddress remoteIpAddress = connection.RemoteIpAddress;
-
-                await ctx.Response.WriteAsync("IP Address: " + remoteIpAddress.ToString());
+                await ctx.Response.WriteAsync("IP Address: " + ipAddress);
             });
         }
     }
