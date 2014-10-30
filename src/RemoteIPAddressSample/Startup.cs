b3abc310c77d642cd60738427e1ea1a4de6b0ac5
tugberkugurlu <tugberkugurlu@gmail.com>
2014-10-30 12:19:39 +0200

    refactored into an extension method: GetClientIPAddress

--- src/RemoteIPAddressSample/Startup.cs
+++ src/RemoteIPAddressSample/Startup.cs
@@ -1,7 +1,8 @@
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Http;
-using System.Threading.Tasks;
 using Microsoft.AspNet.HttpFeature;
+using System.Threading.Tasks;
+using System;
 
 namespace RemoteIPAddressSample
 {
@@ -11,15 +12,27 @@ namespace RemoteIPAddressSample
         {
             app.Run(async (ctx) =>
             {
-                // https://github.com/aspnet/HttpAbstractions/blob/fee220569aa108078ab0e231080724eb74ec8b2d/src/Microsoft.AspNet.Http/HttpContext.cs#L45-L48
-                // https://github.com/aspnet/HttpAbstractions/blob/fee220569aa108078ab0e231080724eb74ec8b2d/src/Microsoft.AspNet.HttpFeature/IHttpConnectionFeature.cs
-                IHttpConnectionFeature connection = ctx.GetFeature<IHttpConnectionFeature>();
-                string ipAddress = connection != null
-                    ? connection.RemoteIpAddress.ToString()
-                    : null;
-
-                await ctx.Response.WriteAsync("IP Address: " + ipAddress);
+                await ctx.Response.WriteAsync("IP Address: " + ctx.GetClientIPAddress());
             });
+        }
+    }
+
+    public static class HttpContextExtensions
+    {
+        public static string GetClientIPAddress(this HttpContext context)
+        {
+            if(context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
+            // https://github.com/aspnet/HttpAbstractions/blob/fee220569aa108078ab0e231080724eb74ec8b2d/src/Microsoft.AspNet.Http/HttpContext.cs#L45-L48
+            // https://github.com/aspnet/HttpAbstractions/blob/fee220569aa108078ab0e231080724eb74ec8b2d/src/Microsoft.AspNet.HttpFeature/IHttpConnectionFeature.cs
+            IHttpConnectionFeature connection = context.GetFeature<IHttpConnectionFeature>();
+
+            return connection != null
+                ? connection.RemoteIpAddress.ToString()
+                : null;
         }
     }
 }
