59115488f6d0f77f7c6964a004ca055db866490d
Tugberk Ugurlu <tugberk@outlook.com>
2015-10-27 20:43:35 +0000

    added serilog

--- beta8/LoggingCorrelationSample/Startup.cs
+++ beta8/LoggingCorrelationSample/Startup.cs
@@ -2,29 +2,31 @@ using System;
 using Microsoft.AspNet.Builder;
 using Microsoft.Framework.DependencyInjection;
 using Microsoft.Framework.Logging;
+using Serilog;
 
 namespace LoggingCorrelationSample
 {
-	public class Startup 
-	{	
+    public class Startup 
+	{
+		public Startup(ILoggerFactory loggerFactory)
+		{
+			var serilogLogger = new LoggerConfiguration()
+				.WriteTo
+				.TextWriter(Console.Out, outputTemplate: "{RequestId}: {Message}{NewLine}")
+				.MinimumLevel.Verbose()
+				.CreateLogger();
+				
+			loggerFactory.MinimumLevel = LogLevel.Verbose;
+			loggerFactory.AddSerilog(serilogLogger);
+		}
+		
 		public void ConfigureServices(IServiceCollection services)
         {
 			services.AddMvc();
 		}
 		
-        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app)
 		{
-			loggerFactory.AddConsole(minLevel: LogLevel.Verbose);
-			
-			app.Use(async (ctx, next) => 
-			{
-				var logger = loggerFactory.CreateLogger("temp");
-				using(logger.BeginScope(Guid.NewGuid().ToString()))
-				{
-					await next();	
-				}
-			});
-			
 			app.UseMvc();
 		}
 	}
