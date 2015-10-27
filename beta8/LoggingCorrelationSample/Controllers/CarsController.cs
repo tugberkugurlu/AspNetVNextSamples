a4a6caace7af85be4391327d049984297ed7ea7d
Tugberk Ugurlu <tugberk@outlook.com>
2015-10-27 22:15:47 +0000

    extend logging to app

--- beta8/LoggingCorrelationSample/Controllers/CarsController.cs
+++ beta8/LoggingCorrelationSample/Controllers/CarsController.cs
@@ -1,18 +1,52 @@
+using System;
+using System.Collections.Generic;
 using Microsoft.AspNet.Mvc;
+using Microsoft.Framework.Logging;
 
 namespace LoggingCorrelationSample
 {
-	public class CarsController : Controller 
-	{
-		[Route("cars")]
-		public IActionResult Get()
+    public class CarsContext : IDisposable
+    {
+        private readonly ILogger _logger;
+
+        public CarsContext(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<CarsContext>();
+            _logger.LogDebug("Constructing CarsContext");
+        }
+
+        public IEnumerable<string> GetCars()
 		{
-			return Ok(new []
-			{
-				"Car 1",
-				"Car 2",
-				"Car 3"
-			});
+			_logger.LogInformation("Found 3 cars.");
+			
+			return new[]
+            {
+                "Car 1",
+                "Car 2",
+                "Car 3"
+            };
 		}
-	}
+        
+        public void Dispose()
+        {
+            _logger.LogDebug("Disposing CarsContext");
+        }
+    }
+
+    public class CarsController : Controller
+    {
+		private readonly CarsContext _carsContext;
+		
+		public CarsController(CarsContext carsContext)
+		{
+			 _carsContext = carsContext;
+		}
+		
+        [Route("cars")]
+        public IActionResult Get()
+        {
+            var cars = _carsContext.GetCars();
+            return Ok(cars);
+        }
+    }
 }
--- beta8/LoggingCorrelationSample/Startup.cs
+++ beta8/LoggingCorrelationSample/Startup.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
 using Microsoft.Framework.DependencyInjection;
 using Microsoft.Framework.Logging;
 using Serilog;
@@ -23,11 +25,31 @@ namespace LoggingCorrelationSample
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddScoped<CarsContext, CarsContext>();
         }
 
         public void Configure(IApplicationBuilder app)
         {
+            app.UseMiddleware<RequestUrlLoggerMiddleware>();
             app.UseMvc();
+        }
+    }
+    
+    public class RequestUrlLoggerMiddleware 
+    {
+        private readonly RequestDelegate _next;
+        private readonly Microsoft.Framework.Logging.ILogger _logger;
+        
+        public RequestUrlLoggerMiddleware(RequestDelegate next, ILoggerFactory loggerFactory) 
+        {
+            _next = next;
+            _logger = loggerFactory.CreateLogger<RequestUrlLoggerMiddleware>();
+        }
+        
+        public Task Invoke (HttpContext context)
+        {
+            _logger.LogInformation("{Method}: {Url}", context.Request.Method, context.Request.Path);
+            return _next(context);
         }
     }
 }
